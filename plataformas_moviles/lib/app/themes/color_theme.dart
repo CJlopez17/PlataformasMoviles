1292436270c1c6ed56478704c8bca426ebd6c99c
Camilo López <camilojoloco123@icloud.com>
2025-01-09 09:08:23 -0500

    colores Theme

---
+++ plataformas_moviles/lib/app/themes/color_theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+abstract class LightSchema {
+  static const Color primaryColor = Color(0xFFFE3791);
+  
+  static const Color secondaryColor = Color(0xFFFECD1F);
+
+  static const Color tertiaryColor = Color(0xFF3F3F3F);
+
+  static const Color fourthColor = Color(0xFF7266BA);
+}
+
+abstract class DarkSchema {
+  static const Color primaryColor = Color(0xFFFE3791);
+  
+  static const Color secondaryColor = Color(0xFFFECD1F);
+
+  static const Color tertiaryColor = Color(0xFF3F3F3F);
+
+  static const Color fourthColor = Color(0xFF7266BA);
+}
+
